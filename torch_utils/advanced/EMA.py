a443c11e2b8a25821fe6707cd5e8fd3e37313f0d
SeeFun <seefun@outlook.com>
2020-08-06 11:42:56 +0800

    Update EMA.py

--- torch_utils/advanced/EMA.py
+++ torch_utils/advanced/EMA.py
@@ -47,3 +47,16 @@ def evaluate():
     # evaluate
     ema.restore()
 '''
+
+'''
+# pytorch 1.6.0 +
+ema_avg = lambda averaged_model_parameter, model_parameter, num_averaged:\
+        0.1 * averaged_model_parameter + 0.9 * model_parameter
+ema_model = torch.optim.swa_utils.AveragedModel(model, avg_fn=ema_avg)
+for i in range(EPOCH):
+    xxxx
+    if i > ema_start:
+        ema_model.update_parameters(model)
+torch.optim.swa_utils.update_bn(loader, ema_model)
+preds = ema_model(test_input)
+'''
