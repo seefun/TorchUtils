fc50418934da8dd916ec5b096441084522798b00
SeeFun <seefun@outlook.com>
2020-10-29 18:55:40 +0800

    Update label_smoothing.py

--- torch_utils/criterion/label_smoothing.py
+++ torch_utils/criterion/label_smoothing.py
@@ -51,3 +51,33 @@ class LabelSmoothingCrossEntropy(nn.Module):
         nll = F.nll_loss(log_preds, target, reduction=self.reduction)
         # (1-ε)* H(q,p) + ε*H(u,p)
         return (1-self.eps)*nll + self.eps*(loss/c) 
+ 
+
+from torch.nn.modules.loss import _WeightedLoss
+
+class SmoothBCEwLogits(_WeightedLoss):
+    def __init__(self, weight=None, reduction='mean', smoothing=0.0):
+        super().__init__(weight=weight, reduction=reduction)
+        self.smoothing = smoothing
+        self.seight = weight
+        self.reduction = reduction
+        
+    @staticmethod
+    def _smooth(targets:torch.Tensor, n_labels:int, smoothing=0.0):
+        assert 0<=smoothing<1
+        with torch.no_grad():
+            targets = targets * (1.0-smoothing) + 0.5*smoothing
+        return targets
+    
+    def forward(self, inputs, targets):
+        targets = SmoothBCEwLogits._smooth(targets, inputs.size(-1), self.smoothing)
+        loss = F.binary_cross_entropy_with_logits(inputs, targets, self.weight)
+        
+        if self.reduction == 'sum':
+            loss = loss.sum()
+        elif self.reduction == 'mean':
+            loss = loss.mean()
+        else:
+            loss = loss.mean()
+            
+        return loss
