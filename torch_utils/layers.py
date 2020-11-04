a6d6e3f01b87a0f16b5f91d6b2b4ef7fc9b69430
SeeFun <seefun@outlook.com>
2020-11-04 21:37:56 +0800

    Update layers.py

--- torch_utils/layers.py
+++ torch_utils/layers.py
@@ -158,6 +158,22 @@ class Swish(nn.Module):
             return x
         else:
             return x * torch.sigmoid(x)
+        
+        
+class FReLU(nn.Module):
+    """
+    FReLU formulation. The funnel condition has a window size of kxk. (k=3 by default)
+    """
+    def __init__(self, in_channels):
+        super().__init__()
+        self.conv_frelu = nn.Conv2d(in_channels, in_channels, kernel_size = 3, stride = 1, padding = 1, groups = in_channels)
+        self.bn_frelu = nn.BatchNorm2d(in_channels)
+        
+    def forward(self, x):
+        y = self.conv_frelu(x)
+        y = self.bn_frelu(y)
+        x = torch.max(x, y)
+        return x
 
 
 ###### example ###### 
