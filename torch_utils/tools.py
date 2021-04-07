596b96d1509ee61eaff46dc5fafa882da3812c41
SeeFun <seefun@outlook.com>
2021-04-07 13:05:14 +0800

    add deterministic and benchmark switch

--- torch_utils/tools.py
+++ torch_utils/tools.py
@@ -5,13 +5,16 @@ import numpy as np
 
 import shutil
 
-def seed_everything(seed=42):
+def seed_everything(seed=42, deterministic=True):
     random.seed(seed)
     os.environ['PYHTONHASHSEED'] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+    else:
+        torch.backends.cudnn.benchmark = True
 
 
 def backup_folder(source='.', destination='../exp/exp1/src'):
