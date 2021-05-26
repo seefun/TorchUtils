1dffa5f3114be10e118242cd35bbc9d90dda81b8
DESKTOP-M5SJKG0\seefu <seefun@outlook.com>
2021-05-26 13:13:05 +0800

    adapt to flake8

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+def install_package(package):
+    output = subprocess.check_output(
+        [sys.executable, '-m', 'pip', 'install', package])
+    print(output.decode())
+
 def load_package(requirements_path='requirements.txt'):
     requirements = []
     with open(requirements_path, 'r') as f:
