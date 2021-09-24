f1abd8382354cdfa1a98bd3f55b3a6d511b2b4c1
DESKTOP-M5SJKG0\seefu <seefun@outlook.com>
2021-09-24 15:58:01 +0800

    [feat] deepsup and clshead for UNet

--- torch_utils/models/seg_models/unet.py
+++ torch_utils/models/seg_models/unet.py
@@ -10,7 +10,7 @@ from torch_utils.models import create_timm_model
 class CenterBlock(nn.Module):
     def __init__(self, in_channel, out_channel):
         super().__init__()
-        self.conv = conv3x3(in_channel, out_channel).apply(init_weight)
+        self.conv = conv3x3(in_channel, out_channel)
 
     def forward(self, inputs):
         x = self.conv(inputs)
@@ -20,20 +20,20 @@ class CenterBlock(nn.Module):
 class DecodeBlock(nn.Module):
     def __init__(self, in_channel, out_channel, upsample, attention=None):
         super().__init__()
-        self.bn1 = nn.BatchNorm2d(in_channel).apply(init_weight)
+        self.bn1 = nn.BatchNorm2d(in_channel)
         self.upsample = nn.Sequential()
         if upsample:
             self.upsample.add_module('upsample', nn.Upsample(scale_factor=2, mode='nearest'))
-        self.conv3x3_1 = conv3x3(in_channel, in_channel).apply(init_weight)
-        self.bn2 = nn.BatchNorm2d(in_channel).apply(init_weight)
-        self.conv3x3_2 = conv3x3(in_channel, out_channel).apply(init_weight)
+        self.conv3x3_1 = conv3x3(in_channel, in_channel)
+        self.bn2 = nn.BatchNorm2d(in_channel)
+        self.conv3x3_2 = conv3x3(in_channel, out_channel)
         if attention == 'scse':
             self.attention = SCSE(out_channel, r=8)
         elif attention == 'cbam':
             self.attention = CBAM(out_channel, reduction=16)
         else:
             self.attention = nn.Identity()
-        self.conv1x1 = conv1x1(in_channel, out_channel).apply(init_weight)
+        self.conv1x1 = conv1x1(in_channel, out_channel)
 
     def forward(self, inputs):
         x = F.relu(self.bn1(inputs))
@@ -45,12 +45,13 @@ class DecodeBlock(nn.Module):
         return x
 
 
-def get_encoder_info(name='resnest50d'):
+def get_encoder_info(name='resnest50d', verbose=True):
     model = create_timm_model(name, pretrained=False).eval()
     features = model(torch.rand(1, 3, 224, 224))
     encoder_channels = []
     for i, feat in enumerate(features):
-        print('Feature %d' % i, feat.shape)
+        if verbose:
+            print('Feature %d' % i, feat.shape)
         encoder_channels.append(feat.shape[1])
     return encoder_channels
 
@@ -115,8 +116,9 @@ class UNet_neck(nn.Module):
             y0 = self.decoder0(y1)  # ->(*,64,h,w)
         else:
             y0 = self.decoder0(torch.cat([x0, y1]))  # ->(*,64,h,w)
-        
+
         return [y4, y3, y2, y1, y0]
+
 
 class UNet(nn.Module):
     def __init__(self,
@@ -129,7 +131,9 @@ class UNet(nn.Module):
                  decoder_channels=[64, 64, 64, 64, 64],
                  out_channel=1,
                  attention='cbam',
-                 hypercolumns=True):
+                 hypercolumns=True,
+                 deepsupervision=False,
+                 clshead=False):
         super().__init__()
         self.backbone = create_timm_model(backbone, pretrained)
         self.attention = attention
@@ -151,34 +155,54 @@ class UNet(nn.Module):
                 final_channel = sum(decoder_channels)
             else:
                 final_channel = decoder_channels[-1]
-                
+
             self.final_conv = nn.Sequential(
-                conv3x3(final_channel, decoder_channels[4]).apply(init_weight),
+                conv3x3(final_channel, decoder_channels[4]),
                 nn.SiLU(True),
-                conv1x1(decoder_channels[4], self.out_channel).apply(init_weight)
+                conv1x1(decoder_channels[4], self.out_channel)
             )
         else:
             if hypercolumns:
                 final_channel = sum(encoder_channels)
             else:
                 final_channel = encoder_channels[0]
-   
+
             self.final_conv = nn.Sequential(
-                conv1x1(final_channel, self.out_channel * 4).apply(init_weight),
+                conv1x1(final_channel, self.out_channel * 4),
                 nn.SiLU(True),
-                conv3x3(self.out_channel * 4, self.out_channel).apply(init_weight)
+                conv3x3(self.out_channel * 4, self.out_channel)
             )
-        
+
         # upsample
         self.upsample4 = nn.Upsample(scale_factor=16, mode='bilinear', align_corners=True)
         self.upsample3 = nn.Upsample(scale_factor=8, mode='bilinear', align_corners=True)
         self.upsample2 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
         self.upsample1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
 
+        # deepsupervision
+        self.deepsupervision = deepsupervision
+        if self.deepsupervision:
+            self.deep4 = conv1x1(decoder_channels[0], 1)
+            self.deep3 = conv1x1(decoder_channels[1], 1)
+            self.deep2 = conv1x1(decoder_channels[2], 1)
+            self.deep1 = conv1x1(decoder_channels[3], 1)
+
+        # classification head
+        self.clshead = clshead
+        if self.clshead:
+            self.clshead = nn.Sequential(
+                FastGlobalConcatPool2d(),
+                get_simple_fc(2 * encoder_channels[-1], out_channel)
+            )
+
     @ autocast()
     def forward(self, inputs):
         # encoder
         y0, y1, y2, y3, y4 = self.backbone(inputs)
+        # cls head
+        if self.clshead:
+            logits_clf = self.clshead(y4)
+
         if self.neck:
             y4, y3, y2, y1, y0 = self.neck([y0, y1, y2, y3, y4])
         else:
@@ -189,7 +213,7 @@ class UNet(nn.Module):
             y0 = self.upsample1(y0)  # ->(*,64,h,w)
 
         if self.hypercolumns:
-        # hypercolumns
+            # hypercolumns
             y4 = self.upsample4(y4)  # ->(*,64,h,w)
             y3 = self.upsample3(y3)  # ->(*,64,h,w)
             y2 = self.upsample2(y2)  # ->(*,64,h,w)
@@ -198,7 +222,51 @@ class UNet(nn.Module):
         else:
             hypercol = y0
 
+        if self.deepsupervision:
+            s4 = self.deep4(y4)
+            s3 = self.deep3(y3)
+            s2 = self.deep2(y2)
+            s1 = self.deep1(y1)
+            logits_deeps = [s4, s3, s2, s1]
+
         # final conv
         logits = self.final_conv(hypercol)  # ->(*,1,h,w)
 
+        if self.deepsupervision and self.clshead:
+            return logits, logits_deeps, logits_clf
+
+        if self.clshead:
+            return logits, logits_clf
+
+        if self.deepsupervision:
+            return logits, logits_deeps
+
         return logits
+
+
+def get_hrnet(name, out_channel, pretrained=True):
+    encoder_channels = get_encoder_info(name, False)
+    model = UNet(backbone=name,
+                 pretrained=pretrained,
+                 neck=None,
+                 encoder_channels=encoder_channels,
+                 out_channel=out_channel,
+                 hypercolumns=True)
+    return model
+
+
+def get_unet(name, out_channel, pretrained=True):
+    encoder_channels = get_encoder_info(name, False)
+    model = UNet(backbone=name,
+                 pretrained=pretrained,
+                 neck='unet',
+                 drop_first=True,
+                 encoder_channels=encoder_channels,
+                 center_channel=512,
+                 decoder_channels=[64, 64, 64, 64, 64],
+                 out_channel=out_channel,
+                 attention='cbam',
+                 hypercolumns=True,
+                 deepsupervision=False,
+                 clshead=False)
+    return model
