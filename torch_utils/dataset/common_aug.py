bd6587d5587411882547d3ac1063d915beb59db5
DESKTOP-M5SJKG0\seefu <seefun@outlook.com>
2021-10-08 15:20:01 +0800

    [feat] aug adapted to albumentation 1.1.0

--- torch_utils/dataset/common_aug.py
+++ torch_utils/dataset/common_aug.py
@@ -21,25 +21,24 @@ train_transform = albumentations.Compose([
     albumentations.Transpose(p=0.5),
     albumentations.Flip(p=0.5),
     albumentations.OneOf([
-        albumentations.RandomBrightness(0.2, p=1),
-        albumentations.RandomContrast(0.2, p=1),
+        albumentations.RandomBrightnessContrast(0.2, 0.2, p=1),
         albumentations.HueSaturationValue(hue_shift_limit=15, sat_shift_limit=20, val_shift_limit=15, p=1),
-    ], p=0.5),
+    ], p=0.5), 
     albumentations.OneOf([
         albumentations.ElasticTransform(alpha=1, sigma=20, alpha_affine=10),
         albumentations.GridDistortion(num_steps=6, distort_limit=0.1),
         albumentations.OpticalDistortion(distort_limit=0.05, shift_limit=0.05),
-    ], p=0.25),
+    ], p=0.25), 
     albumentations.OneOf([
         albumentations.CLAHE(),
         albumentations.ISONoise(color_shift=(0.01, 0.05), intensity=(0.1, 0.3)),
-        albumentations.IAASharpen(alpha=(0.1, 0.3), lightness=(0.5, 1.0)),
+        albumentations.Sharpen(alpha=(0.1, 0.3), lightness=(0.5, 1.0)),
         albumentations.GaussNoise((5, 30)),
-        albumentations.JpegCompression(30, 90),
-    ], p=0.5),
+        albumentations.ImageCompression(30, 90),
+    ], p=0.5), 
     albumentations.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.0625, rotate_limit=45, border_mode=1, p=0.5),
     albumentations.Normalize(),
-    albumentations.Cutout(num_holes=8, max_h_size=IMAGE_SIZE // 8, max_w_size=IMAGE_SIZE // 8, fill_value=0, p=0.25),
+    albumentations.CoarseDropout(max_holes=8, max_height=IMAGE_SIZE // 8, max_width=IMAGE_SIZE // 8, fill_value=0, p=0.25),
     AT.ToTensorV2(),
 ])
 
