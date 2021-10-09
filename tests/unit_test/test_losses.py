7f83446980b5b4f2d5d259be56360c1dc3a5b4df
DESKTOP-M5SJKG0\seefu <seefun@outlook.com>
2021-10-09 16:18:42 +0800

    [feat] update test loss

--- tests/unit_test/test_losses.py
+++ tests/unit_test/test_losses.py
@@ -1,6 +1,7 @@
 import torch
 from torch_utils import criterion
 
+
 class TestBinaryLoss:
     y_pred = torch.tensor([0.88, -0.10, 0.55]).view(1, 1, 1, -1)
     y_true = torch.tensor(([1, 0, 1])).view(1, 1, 1, -1)
@@ -33,4 +34,27 @@ class TestBinaryLoss:
     def test_soft_target_ce(self):
         loss_fn = criterion.SoftTargetCrossEntropy()
         loss = loss_fn(TestBinaryLoss.y_pred, TestBinaryLoss.y_true)
+        print(loss)
+
+
+class TestMultiLoss:
+    y_pred = torch.tensor([[+1, -1, -1, -1],
+                           [-1, +1, -1, -1],
+                           [-1, -1, +1, -1],
+                           [-1, -1, -1, +1]]).float()
+    y_true = torch.tensor([0, 1, 2, 3]).long()
+
+    def test_focal(self):
+        loss_fn = criterion.FocalLoss()
+        loss = loss_fn(TestMultiLoss.y_pred, TestMultiLoss.y_true)
+        print(loss)
+
+    def test_lovasz(self):
+        loss_fn = criterion.LovaszLoss()
+        loss = loss_fn(TestMultiLoss.y_pred, TestMultiLoss.y_true)
+        print(loss)
+
+    def test_smoothing(self):
+        loss_fn = criterion.LabelSmoothingCrossEntropy()
+        loss = loss_fn(TestMultiLoss.y_pred, TestMultiLoss.y_true)
         print(loss)
