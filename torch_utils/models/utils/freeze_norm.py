478971da6a369ed056a67f75e29bcf0196b49965
DESKTOP-M5SJKG0\seefu <seefun@outlook.com>
2021-10-21 17:42:13 +0800

    [feat] add unfreeze

--- torch_utils/models/utils/freeze_norm.py
+++ torch_utils/models/utils/freeze_norm.py
@@ -4,8 +4,19 @@ def set_bn_eval(m):
         m.eval()
 
 
+def set_bn_train(m):
+    classname = m.__class__.__name__
+    if classname.find('BatchNorm') != -1:
+        m.train()
+
+
 def freeze_bn(model):
     for m in model.named_modules():
         set_bn_eval(m[1])
+
+
+def unfreeze_bn(model):
+    for m in model.named_modules():
+        set_bn_train(m[1])
 
 # usage: model.apply(freeze_bn) # this will freeze the bn in training process
